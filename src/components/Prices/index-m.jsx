6ae1a7067db13016699c21f895c4e4cd1b700c97
Asma Kolaib <as.kolaib@gmail.com>
2023-12-19 14:22:07 +0300

    feat add price section mobile

--- src/components/Prices/index-m.jsx
+++ src/components/Prices/index-m.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const Prices = () => {
+  const [plan, setPlan] = useState(
+    " Computer vision | 2D BBox (Poly,Line,Circle,..)"
+  );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="w-full py-8 bg-white font-main ">
       <h1 className="text-center font-extrabold capitalize text-3xl block w-full ">
@@ -13,66 +22,125 @@ const Prices = () => {
           works for you
         </p>
       </div>
+      <div className=" border-none flex flex-row justify-between items-center w-full p-4 ">
+        <div className="w-2/4">{plan}</div>
+        <div>
+          {" "}
+          <div className="relative inline-block">
+            <button
+              onClick={toggleDropdown}
+              type="button"
+              className="inline-flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring focus:border-blue-300"
+              id="options-menu"
+              aria-haspopup="true"
+              aria-expanded="true"
+            >
+              change the plan
+            </button>
 
-      <div className="flex justify-center items-center my-16 overflow-auto">
-        <table className=" border-collapse text-center  text-xs">
-          <thead>
-            <tr>
-              <th colspan="2" className="w-32"></th>
-              <th className=" font-normal   text-left ">Number of Objects</th>
-              <th className=" font-normal   text-left  ">
-                Annotations Per Object
-              </th>
-              <th className=" font-normal   text-left  ">Time Frame in Days</th>
-              <th className=" font-normal   text-center   ">Price</th>
-            </tr>
-          </thead>
+            {isOpen && (
+              <div
+                className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="options-menu"
+              >
+                <div className="py-1" role="none">
+                  <button
+                    onClick={() => {
+                      setPlan(
+                        "Computer vision | 2D BBox (Poly,Line,Circle,..)"
+                      );
+                    }}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    Computer vision | 2D BBox (Poly,Line,Circle,..)
+                  </button>
+                  <button
+                    onClick={() => {
+                      setPlan("Computer vision | Segmentation");
+                    }}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    Computer vision | Segmentation
+                  </button>
+                  <button
+                    onClick={() => {
+                      setPlan(
+                        "Natural Language Processing | Named Entity Recognition"
+                      );
+                    }}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    Natural Language Processing | Named Entity Recognition
+                  </button>
+                  <button
+                    onClick={() => {
+                      setPlan(
+                        "Natural Language Processing | Named Entity Relations"
+                      );
+                    }}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    Natural Language Processing | Named Entity Relations
+                  </button>
+                  <button
+                    onClick={() => {
+                      setPlan(
+                        "others | multi-class annotation (emotion-sentiment-topic,...)"
+                      );
+                    }}
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    others | multi-class annotation (emotion-
+                    sentiment-topic,...)
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center p-4">
+        <table className=" border-collapse text-left  text-base w-full">
           <tbody>
             <tr>
-              <td rowspan="2" className="border-r-2 text-left ">
-                Computer vision
-              </td>
-              <td className="text-left ">2D BBox (Poly,Line,Circle,..)</td>
-              <td>1000</td>
-              <td>10</td>
-              <td>7</td>
-              <td className="font-bold text-base">950$</td>
+              <td className="w-2/4">Number of Objects</td>
+              <td className="w-2/4">1000</td>
             </tr>
             <tr>
-              <td className="text-left">Segmentation</td>
-              <td>1000</td>
-              <td>10</td>
-              <td>7</td>
-              <td className="font-bold text-base">1200$</td>
+              <td className="w-2/4">Annotations Per Object</td>
+              <td className="w-2/4">10</td>
             </tr>
-            <tr className="bg-blue-50">
-              <td rowspan="2" className="text-left w-">
-                Natural Language Processing
-              </td>
-              <td className="text-left">Named Entity Recognition</td>
-              <td>1000</td>
-              <td>10</td>
-              <td>7</td>
-              <td className="font-bold text-base">900$</td>
+            <tr>
+              <td className="w-2/4">Time Frame in Days</td>
+
+              {plan ===
+              "others | multi-class annotation (emotion-sentiment-topic,...)" ? (
+                <td className="w-2/4">1</td>
+              ) : (
+                <td className="w-2/4">7</td>
+              )}
             </tr>
-            <tr className="bg-blue-50">
-              <td className="text-left">Named Entity Relations</td>
-              <td>1000</td>
-              <td>10</td>
-              <td>7</td>
-              <td className="font-bold text-base">950$</td>
-            </tr>
-            <tr className="pt-10">
-              <td rowspan="2" className="text-left">
-                others
-              </td>
-              <td className="text-left    ">
-                multi-class annotation (emotion- sentiment-topic,...)
-              </td>
-              <td>1000</td>
-              <td>10</td>
-              <td>1</td>
-              <td className="font-bold text-base">2000$</td>
+            <tr>
+              <td className="w-2/4">Price</td>
+
+              {plan === "Computer vision | 2D BBox (Poly,Line,Circle,..)" ? (
+                <td className="font-bold w-2/4">950$</td>
+              ) : plan === "Computer vision | Segmentation" ? (
+                <td className="font-bold  w-2/4">1200$</td>
+              ) : plan ===
+                "Natural Language Processing | Named Entity Recognition" ? (
+                <td className="font-bold  w-2/4">900$</td>
+              ) : plan ===
+                "Natural Language Processing | Named Entity Relations" ? (
+                <td className="font-bold  w-2/4">950$</td>
+              ) : plan ===
+                "others | multi-class annotation (emotion-sentiment-topic,...)" ? (
+                <td className="font-bold  w-2/4">2000$</td>
+              ) : (
+                <td className="font-bold w-2/4">950$</td>
+              )}
             </tr>
           </tbody>
         </table>
